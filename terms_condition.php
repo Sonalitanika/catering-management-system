75a10d45c83aed88437e3ddb5136f6f5041b7e93
Sonalitanika <sonalitanika95@gmail.com>
2018-09-08 18:43:00 +0530

    Initial commit

---
+++ terms_condition.php
@@ -0,0 +1,6 @@
+<div style="text-align:center; font-weight:bold;">Terms and Condition</div><br />
+<p>1. Catering is within the area of NUWARAELLIYA only.</p>
+<p>2. Additional 10% payment on reservation outside NUWARAELLIYA.</p>
+<p>3. MINIMUM Pax For catering is 50</p>
+<p>4. Costumer must pay 50% of the actual price as an advance payment 3 days after confirmation, if the costumer failed to pay the said advance payment reservation will be cancelled.</p>
+<p>5. If the costumer wants to cancel its confirmed reservation due to personal reason, the management will get 20% from the advance payment he/she made as charge for the damages.</p>
